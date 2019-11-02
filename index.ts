c45781b4c2c4bcb95fb68e1143740f76a1d58540
Jonas Hecht <jonas.hecht@codecentric.de>
2019-11-02 21:08:18 +0100

    Working and accessible deployment of spring-boot-vuejs into AWS Fargate incl. docs

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// Create a load balancer to listen for requests and route them to the container.
+let loadbalancer = new awsx.lb.ApplicationListener("nginx", { port: 8098, protocol: "HTTP" });
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Define the service using the example Spring Boot Vue.js app from https://github.com/jonashackt/spring-boot-vuejs
+let service = new awsx.ecs.FargateService("spring-boot-vuejs", {
+    taskDefinitionArgs: {
+        containers: {
+            nginx: {
+                image: "jonashackt/spring-boot-vuejs",
+                memory: 512,
+                portMappings: [ loadbalancer ],
+            },
+        },
+    },
+    desiredCount: 2,
+});
+
+// Export the URL so we can easily access it.
+export const url = loadbalancer.endpoint.hostname;
